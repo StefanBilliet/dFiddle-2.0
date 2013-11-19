962a7d94820fd67b73235889fec13b328e5055c7
Stefan Billiet <sbilliet@csc.com>
2013-11-19 12:04:27 +0100

    Fixed typo in durandal paths

--- app/main.js
+++ app/main.js
@@ -34,6 +34,6 @@ define(['durandal/system', 'durandal/app', 'durandal/viewLocator'], function (sy
     viewLocator.useConvention();
 
     //Show the app by setting the root view model for our application with a transition.
-    app.setRoot('Shell/ViewModels/Shell', 'entrance');
+    app.setRoot('shell/viewmodels/shell', 'entrance');
   });
 });
